9f07ccc74e0532874fab1a426261ae8b7e22d549
Edwin Vlieg <e.vlieg@bluetools.nl>
2011-01-20 14:48:47 +0100

    Fix for string length

--- lib/postgresql_validations/limit_validator.rb
+++ lib/postgresql_validations/limit_validator.rb
@@ -7,7 +7,7 @@ module PostgresqlValidations
         case column.type
         when :string
           next if record[column.name].blank?
-          record.errors.add(column.name, :too_long, :count => column.limit) if record[column.name].length > column.limit
+          record.errors.add(column.name, :too_long, :count => column.limit) if record[column.name].to_s.length > column.limit
         when :integer
           next if record[column.name].blank?
           record.errors.add(column.name, :greater_than_or_equal_to, :count => -2147483648) if record[column.name] < -2147483648
